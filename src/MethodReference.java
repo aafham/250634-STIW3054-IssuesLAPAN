e00e93c61a39ef1e2e3c17ff69e5c565bb12cfbf
Afham Azizan <aafham21@gmail.com>
2020-05-06 18:15:44 +0800

    update commit

--- src/MethodReference.java
+++ src/MethodReference.java
@@ -1,47 +1,34 @@
-class ThreadOne implements Runnable {
-    public void run() {
-        try {
-            for(int i=0;i<10;i++) {
-                if(!(i%2==1)){
-                    Thread.sleep(500);
-                    System.out.println("Thread 1: "+i);
-                }
-            }
-        }catch(InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
+public class MethodReference {
+    public static void main(String [] args){
 
-class ThreadTwo implements Runnable {
-    public void run() {
-        try {
-            for(int i=0;i<10;i++) {
-                if(!(i%2==0)) {
-                    Thread.sleep(500);
-                    System.out.println("Thread 2: "+i);
-                }
-            }
-        }catch(InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
+        Thread t1 = new Thread(MethodReference::thread1Even);
+        Thread t2 = new Thread(MethodReference::thread2Odd);
 
-class MethodReference {
-    public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(new ThreadOne());
         t1.start();
-        t1.join();
-
-        Thread t2 = new Thread(new ThreadTwo());
         t2.start();
-        t2.join();
+    }
+    public static void thread1Even(){
+        long start =System.currentTimeMillis();
+        for(int i=0; i<10; i+=2) {
+            System.out.println(Thread.currentThread().getName()+": "+i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        long end =System.currentTimeMillis();
+        System.out.println("Execution Time = "+(end-start)/1000+"s");
+    }
 
-        long startTime = System.currentTimeMillis();
-        long endTime = System.currentTimeMillis();
-        long timeSpend = endTime - startTime;
-        System.out.println();
-        System.out.println("Execution Time ="+timeSpend+ "ms");
+    public static void thread2Odd(){
+        for(int i=1; i<10; i+=2){
+            System.out.println(Thread.currentThread().getName()+": "+i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
